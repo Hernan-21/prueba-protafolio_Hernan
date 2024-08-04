057568f580ee773490eb0ef1a5491abc3f324690
HernanWM <watza.mango@gmail.com>
2024-08-04 18:05:13 -0400

    agregando rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#home'  # Esto define la ruta raíz
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get '/home', to: 'pages#home', as: 'home'
+  get '/projects', to: 'pages#projects', as: 'projects'
+  get '/contact', to: 'pages#contact', as: 'contact'
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
